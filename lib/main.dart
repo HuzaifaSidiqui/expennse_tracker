c9ab96a921c58fab8fbaf9ed369ea68a9197e7e9
HuzaifaSidiqui <huzaifa.siddique5000@gmail.com>
2025-08-21 23:22:24 +0500

    added charts and applied logic

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,30 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
-  seedColor: Color.fromARGB(25, 96, 59, 81),
+  seedColor: Color.fromARGB(25, 96, 59, 181),
+);
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: Color.fromARGB(255, 5, 99, 125),
 );
 
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: CardThemeData().copyWith(
+          color: kDarkColorScheme.primaryContainer,
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
         appBarTheme: const AppBarTheme().copyWith(
@@ -32,6 +50,7 @@ void main() {
           ),
         ),
       ),
+      // themeMode: ThemeMode.system,
       home: const Expenses(),
     ),
   );
